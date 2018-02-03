ed6726a8074e755a64a9df2f390ea0aa3ee36d15
Ioannis G <jtgftw@gmail.com>
2018-02-04 00:45:43 +0200

    add API methods

--- Wox.Plugin.Bang/Plugin.fs
+++ Wox.Plugin.Bang/Plugin.fs
@@ -1,6 +1,56 @@
 namespace Wox.Plugin.Bang
 
 //--------------------------------------------
+//  DuckDuckGo API
+//--------------------------------------------
+
+open Newtonsoft.Json
+open RestSharp
+
+type AutoCompleteSuggestion =
+    { phrase        : string
+      score         : int
+      snippet       : string
+      image         : string }
+
+type BangResult =
+    { Redirect      : string }
+
+type BangClient() =
+
+    let getBangSuggestions bang = async {
+    
+        let restClient = RestClient "https://duckduckgo.com/"
+
+        let req = RestRequest ( "ac/", Method.GET )
+    
+        req.AddParameter ("q", bang)
+    
+        let! resp = 
+            restClient.ExecuteTaskAsync req 
+            |> Async.AwaitTask
+    
+        return JsonConvert.DeserializeObject<AutoCompleteSuggestion list> resp.Content
+    }
+
+    let getBangSearchResults search = async {
+
+        let restClient = RestClient "https://api.duckduckgo.com/"
+
+        let req = RestRequest ( "/", Method.GET )
+    
+        req.AddParameter ("q", search)
+        req.AddParameter ("format", "json")
+        req.AddParameter ("no_redirect", 1)
+    
+        let! resp = 
+            restClient.ExecuteTaskAsync req 
+            |> Async.AwaitTask
+    
+        return JsonConvert.DeserializeObject<BangResult> resp.Content
+    }
+
+//--------------------------------------------
 //  Plugin
 //--------------------------------------------
 
@@ -16,5 +66,5 @@ type BangPlugin() =
             PluginContext <- context
 
         member this.Query (q:Query) =
-            
+        
             List<Result> [ Result ( Title = "", SubTitle = "" ) ]
