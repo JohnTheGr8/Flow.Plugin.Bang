82349e629e4531d4dc4c3f53a9ba63c893d93c1f
Ioannis G <jtgftw@gmail.com>
2020-04-21 17:40:10 +0300

    add tests for various wox query cases

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -4,6 +4,10 @@ open Expecto
 open Expecto.Flip
 open Wox.Plugin.Bang
 
+let checkQuery : _ -> Async<Wox.Plugin.Result list> =
+    let doNothing = fun _ -> false
+    QueryImpl.handleQuery doNothing doNothing
+
 let allTests =
     testList "all tests" [
 
@@ -49,4 +53,38 @@ let allTests =
 
             result.Redirect |> Expect.equal "redirect URL should match" "https://github.com/search?utf8=%E2%9C%93&q=Wox%20Plugin"
         }
+
+        testList "QueryImpl.handleQuery" [
+
+            testAsync "no query terms" {
+                let! results = checkQuery []
+
+                results |> Expect.isEmpty "should be empty"
+            }
+
+            testAsync "just a bang phrase" {
+                let! results = checkQuery [ "!imdb" ]
+                let actual = List.tryHead results
+
+                actual                  |> Expect.isSome "results should not be empty"
+                actual.Value.Title      |> Expect.equal "result title should equal"    "!imdb"
+                actual.Value.SubTitle   |> Expect.equal "result subtitle should equal" "Search IMDB"
+            }
+
+            testAsync "bang phrase and space" {
+                let! results = checkQuery [ "!tw"; "" ]
+                let actual = List.tryExactlyOne results
+
+                actual              |> Expect.isSome "there should be exactly one result"
+                actual.Value.Title  |> Expect.equal "result subtitle should equal" "Search Twitter"
+            }
+
+            testAsync "bang phrase and search" {
+                let! results = checkQuery [ "!gh"; "Wox" ]
+                let actual = List.tryExactlyOne results
+
+                actual                  |> Expect.isSome "there should be exactly one result"
+                actual.Value.SubTitle   |> Expect.equal "redirect URL should match" "https://github.com/search?utf8=%E2%9C%93&q=Wox"
+            }
+        ]
     ]
